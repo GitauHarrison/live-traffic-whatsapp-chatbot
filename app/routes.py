518738ce47f9ce4ccaae963a060734a9f75fa9c0
harry <harrisongitau3@gmail.com>
2021-04-03 05:35:38 +0300

    Get traffic data

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+from twilio.twiml.messaging_response import MessagingResponse
+import folium
+import requests
+
+
+def get_traffic_data(lat, lon):
+    params = {'points': f'{lat},{lon}',
+              'unit': 'kmph',
+              'thickness': 14,
+              'key': app.config['TOMTOM_API_KEY']
+              },
+    base_url = 'https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json'
+    data = requests.get(base_url, params=params).json()
+    return data
 
 
 @app.route('/')
